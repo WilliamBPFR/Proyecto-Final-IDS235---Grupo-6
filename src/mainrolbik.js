12202445dba974503cca2f1a0d1416ec87e04a69
Rolbik Miguel Urbaez Ferreras <1105721@est.intec.edu.do>
2023-06-29 03:42:02 -0400

    Avance crear asignatura

--- src/mainrolbik.js
+++ src/mainrolbik.js
@@ -29,5 +29,34 @@ app.listen(process.env.PORT,()=>{
 });
 
 app.get('/',(req,res)=>{
-    res.redirect('/iniciar_sesion.html');
+    res.redirect('/crear_asignatura.html');
+});
+
+app.post('/crear-asignatura',async (req,res,next)=>{
+    const codigo_asignatura = req.body.txt_Cod_Asignatura;
+    const creditos_asignatura = req.body.txt_Creditos_Asignatura;
+    const carrera_asignatura = req.body.sel_Carrera_Pertenece;
+    const nombre_asignatura = req.body.txt_Nombre_Asignatura;
+    const tipo_asignatura = req.body.sel_Asignatura_Tipo;
+    const asignatura_visible = req.body.sel_Asignatura_Visible;
+
+    try{
+        const new_state = await prisma.asignatura.create({
+            data:
+            {                    
+                codigo_asignatura:cod_asignatura,                    
+                creditos_asignatura:creditos,
+                carrera_asignatura:carrera,
+                nombre_asignatura:nombre_asignacion,
+                tipo_asignatura:id_tipo_asignatura,
+                asignatura_visible:visible
+            }
+        });
+        const estados = await prisma.estado.findMany();
+        console.log(estados);
+        res.send('Asignatura creada exitosamente');
+    }catch(error){
+        console.log(error);
+        res.status(500).send('Ocurrió un error al crear la asignatura');
+    }
 });
