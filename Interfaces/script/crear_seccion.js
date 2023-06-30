5a6d57e992929baad6daedb1f28a6cdd7a1dc5e2
Cristian Jimenez <1106166@est.intec.edu.do>
2023-06-29 22:18:31 -0400

    cambios seccion

---
+++ Interfaces/script/crear_seccion.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Asociar el controlador de eventos al botón
+    $('#btn_Guardar').click(function() {
+      // Aquí puedes ejecutar la función que deseas cuando se hace clic en el botón
+      window.location.href = 'cargar_crear_seccion/';
+    });
+  });
+  
